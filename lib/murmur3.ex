40612e58cb97507e0c8475dea2af582f199cfc30
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2024-02-15 10:31:17 -0600

    Initial commit

---
+++ lib/murmur3.ex
@@ -0,0 +1,18 @@
+defmodule Murmur3 do
+  @moduledoc """
+  Documentation for `Murmur3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Murmur3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
