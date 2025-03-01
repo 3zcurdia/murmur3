6a10d9c6ca2147468276867b764ea8edaed67dac
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2025-02-28 23:39:02 -0600

    Update mix packages

--- mix.exs
+++ mix.exs
@@ -4,7 +4,14 @@ defmodule Murmur3.MixProject do
   def project do
     [
       app: :murmur3,
-      version: "0.1.1",
+      compilers: Mix.compilers(),
+      rustler_crates: [
+        expolars: [
+          path: "native/murmur3_nif",
+          mode: if(Mix.env() == :prod, do: :release, else: :debug)
+        ]
+      ],
+      version: "0.1.2",
       elixir: ">= 1.10.0",
       source_url: "https://github.com/3zcurdia/murmur3",
       description: "Nif Wrapper for Murmur3 rust library",
@@ -37,10 +44,8 @@ defmodule Murmur3.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.36.0"},
-      {:ex_doc, "~> 0.37.0", only: :dev}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.36.1"},
+      {:ex_doc, "~> 0.37.2", only: :dev}
     ]
   end
 
